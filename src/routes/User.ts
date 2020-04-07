caefd72c0deb2bd18516ff78978e25d2c15b5967
Cavs Zhouyou <zhouyou.world@outlook.com>
2020-04-07 14:32:55 +0800

    feat: 新增 resetPassword 接口

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -152,4 +152,32 @@ router.post('/deleteUser', async (req: Request, res: Response) => {
     success: true,
   });
 });
+
+router.post('/resetPassword', async (req: Request, res: Response) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res.status(OK).json({
+      success: false,
+      message: paramMissingError,
+    });
+  }
+
+  const user = await userRepository.findOne({ userId });
+
+  if (!user) {
+    return res.status(OK).json({
+      success: false,
+      message: '用户不存在！',
+    });
+  }
+
+  user.pwdHash = bcrypt.hashSync(md5(userId), pwdSaltRounds);
+
+  await userRepository.save(user);
+
+  return res.status(OK).json({
+    success: true,
+  });
+});
 export default router;
