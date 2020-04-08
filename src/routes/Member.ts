613c7a9caebfc44b272b8602725375b906be037d
Cavs Zhouyou <zhouyou.world@outlook.com>
2020-04-08 20:11:53 +0800

    chore: 完成 addApp

--- src/routes/Member.ts
+++ src/routes/Member.ts
@@ -8,9 +8,86 @@ import {
   memberRoleRepository,
   memberRepository,
 } from '@shared/repositories';
+import { Member } from '@entity/Member';
 
 // Init shared
 const router = Router().use(loginMW);
+
+/******************************************************************************
+ *            获取应用成员列表 - "POST/def/member/getAppMemberList"
+ ******************************************************************************/
+
+router.post('/getAppMemberList', async (req: Request, res: Response) => {
+  const { appId, page, pageSize } = req.body;
+
+  if (!(appId && page && pageSize)) {
+    return res.status(OK).json({
+      success: false,
+      message: paramMissingError,
+    });
+  }
+
+  const app = await appRepository.findOne({
+    appId,
+  });
+
+  let members: any[] = [];
+  let hasMore = true;
+  let total = 0;
+  const dataStart = (page - 1) * pageSize;
+  const relations = ['role', 'user'];
+
+  // 用户列表查询
+  const originMembers = await memberRepository.find({
+    where: {
+      app,
+    },
+    relations,
+  });
+  total = members.length;
+
+  originMembers.forEach((item: Member) => {
+    const { user, role, joinTime, expiredTime } = item;
+    const { userId, userName, userAvatar } = user;
+
+    if (parseInt(expiredTime) > new Date().getTime()) {
+      members.push({
+        userId,
+        userName,
+        userAvatar,
+        role,
+        joinTime,
+        expiredTime,
+      });
+    }
+  });
+
+  if (dataStart > total) {
+    return res.status(OK).json({
+      success: false,
+      message: '超出数据范围！',
+    });
+  } else {
+    hasMore = dataStart + pageSize < total;
+
+    if (hasMore) {
+      members = members.slice(dataStart, pageSize);
+    } else {
+      members = members.slice(dataStart);
+    }
+  }
+
+  return res.status(OK).json({
+    success: true,
+    data: {
+      page,
+      pageSize,
+      hasMore,
+      total,
+      list: members,
+    },
+  });
+});
 
 /******************************************************************************
  *            添加应用成员 - "POST/def/member/addAppMember"
@@ -30,7 +107,7 @@ router.post('/addAppMember', async (req: Request, res: Response) => {
     appId,
   });
   const memberRole = await memberRoleRepository.findOne({
-    code: role,
+    roleId: role,
   });
   const user = await userRepository.findOne({
     userName,
@@ -39,19 +116,38 @@ router.post('/addAppMember', async (req: Request, res: Response) => {
   if (!user) {
     return res.status(OK).json({
       success: false,
-      message: '用户不存在',
+      message: '用户不存在！',
     });
   }
 
-  const member = memberRepository.create({
-    joinTime: new Date().getTime(),
-    expiredTime: new Date().getTime() + parseInt(useTime || ''),
-    role: memberRole,
-    app,
+  const member = await memberRepository.findOne({
     user,
   });
 
-  await memberRepository.save(member);
+  if (member) {
+    // 判断用户是否存在，且权限是否过期
+    if (parseInt(member.expiredTime) >= new Date().getTime()) {
+      return res.status(OK).json({
+        success: false,
+        message: '该用户已经加入应用！',
+      });
+    } else {
+      member.role = role;
+      member.expiredTime = new Date().getTime() + parseInt(useTime || '');
+      await memberRepository.save(member);
+    }
+  } else {
+    // 新用户第一次添加
+    const savedMember = memberRepository.create({
+      joinTime: new Date().getTime(),
+      expiredTime: new Date().getTime() + parseInt(useTime || ''),
+      role: memberRole,
+      app,
+      user,
+    });
+
+    await memberRepository.save(savedMember);
+  }
 
   return res.status(OK).json({
     success: true,
